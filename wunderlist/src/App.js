33b77a1a7c0e4ffa2a88f9fa2d2355e7f78c2700
Leonel <leonelaf12@gmail.com>
2019-05-20 13:37:05 -0400

    commit for Thomas

--- wunderlist/src/App.js
+++ wunderlist/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Header from './components/Header';
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import Todos from './components/Todos'
 
 function App() {
   return (
-    <div className="App">
-      
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to = '/login'>Login</Link>
+          </li>
+          <li>
+            <Link to = '/signup'>Sign Up</Link>
+          </li>
+          {/* <li>
+            <Link to = '/tasks' >Tasks</Link>
+          </li> */}
+        </ul>
+      <Route exact patch = '/login' component = {Login}/>
+      <Route exact patch = '/signup' component = {SignUp} />
+      {/* <Route path = '/tasks' component = {Todos} /> */}
   
-    </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ wunderlist/src/components/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function SignUp() {
+    return (
+        <div className= 'signUp-wrapper'>
+            <h1>Sign Up Page</h1>
+        </div>
+    )
+}
+
+export default SignUp;
